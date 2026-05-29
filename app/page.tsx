871ee110daf411be50b9e6de1398bd7ad9730675
matt silverman <silverman.me@gmail.com>
2026-05-29 14:28:13 -0700

    redesign: center MatrixLoader on full screen, remove LoaderSpinner wrapper

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
 import { MatrixLoader } from "@/components/matrix-loader"
-import { LoaderSpinner } from "@/components/loader-spinner"
 
 export default function Page() {
   return (
     <div className="min-h-screen bg-[#101010] flex items-center justify-center">
-      <div className="w-[700px] h-[700px] bg-[#101010] flex items-center justify-start pl-[56px]">
-        <div className="flex items-center gap-[16px]">
-          <LoaderSpinner />
-          <MatrixLoader />
-        </div>
-      </div>
+      <MatrixLoader />
     </div>
   )
 }
